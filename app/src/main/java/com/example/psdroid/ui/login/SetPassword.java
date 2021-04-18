117a9d6c4e7e371b4e682450f4d84da46c464072
karn301 <karnsshinde961@gmail.com>
2021-04-18 18:46:35 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/example/psdroid/ui/login/SetPassword.java
@@ -0,0 +1,121 @@
+package com.example.psdroid.ui.login;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.audiofx.BassBoost;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.psdroid.MainScreen;
+import com.example.psdroid.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.net.NetworkInterface;
+
+public class SetPassword extends AppCompatActivity {
+         EditText npass,connpass;
+ImageView cancel;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_password);
+
+        npass = findViewById(R.id.pass);
+        connpass = findViewById(R.id.conpass);
+        cancel = findViewById(R.id.cancel);
+
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SetPassword.this,LoginActivity.class));
+                finish();
+            }
+        });
+
+
+
+
+
+    }
+
+
+    public void setNewPassword(View view){
+        CheckInternet checkInternet = new CheckInternet();
+        if(!checkInternet.isConnected(this)){
+            showCustomDialog();
+            return;
+        }
+        else {
+            String np = npass.getText().toString().trim();
+            String cnp = connpass.getText().toString().trim();
+
+
+         /*   String _user = getIntent().getStringExtra("user");
+            rootNode = FirebaseDatabase.getInstance();
+            reference = rootNode.getReference("users");
+            if (np.equals(cnp) && ) {
+                reference.child(_user).child("pass").setValue(np);
+                reference.child(_user).child("conpass").setValue(cnp);
+                startActivity(new Intent(getApplicationContext(), MainScreen.class));
+                finish();
+            } else */
+            if((np.length()<8) && (cnp.length()<8)) {
+                Toast.makeText(this, "Password too match", Toast.LENGTH_SHORT).show();
+            }
+            else if (!np.equals(cnp)){
+                Toast.makeText(this, "Password does not match", Toast.LENGTH_SHORT).show();
+            }
+            else{
+
+
+
+                String _user = getIntent().getStringExtra("user");
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+                reference.child(_user).child("pass").setValue(np);
+                reference.child(_user).child("conpass").setValue(cnp);
+                startActivity(new Intent(getApplicationContext(), MainScreen.class));
+                finish();
+            }
+        }
+
+
+
+    }
+
+    private void showCustomDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(SetPassword.this);
+        builder.setMessage("Please connect to the internet to proceed further")
+                .setCancelable(false)
+                .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/psdroid/ui/login/VerifyOTP.java
@@ -0,0 +1,178 @@
+package com.example.psdroid.ui.login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActivityChooserView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.chaos.view.PinView;
+import com.example.psdroid.MainScreen;
+import com.example.psdroid.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.concurrent.TimeUnit;
+
+public class VerifyOTP extends AppCompatActivity {
+    PinView pinview;
+    String codebysystem;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+    String WhatToDo;
+    ImageView cancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_o_t_p);
+
+        pinview = findViewById(R.id.pinview);
+        cancel = findViewById(R.id.cross);
+        String _user = getIntent().getStringExtra("user");
+        String _mail = getIntent().getStringExtra("mail");
+        String _mobile = getIntent().getStringExtra("mob");
+        String _pass = getIntent().getStringExtra("pass");
+        String _cpass = getIntent().getStringExtra("cpass");
+        WhatToDo = getIntent().getStringExtra("whatToDo");
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(VerifyOTP.this,LoginActivity.class));
+                finish();
+            }
+        });
+        sendVerificationCodeToUser(_mobile);
+
+
+
+        
+    }
+
+    private void sendVerificationCodeToUser(String mobile) {
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(mobile)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallBack)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
+    }
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBack =
+            new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                @Override
+                public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                    super.onCodeSent(s, forceResendingToken);
+                    codebysystem = s;
+
+                }
+
+                @Override
+                public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        String code = phoneAuthCredential.getSmsCode();
+                        if(code!=null){
+                            pinview.setText(code);
+                            verifyCode(code);
+                        }
+                }
+
+                @Override
+                public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                    Toast.makeText(VerifyOTP.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            };
+
+    private void verifyCode(String code) {
+
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codebysystem,code);
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        
+                        if(("updateData").equalsIgnoreCase(WhatToDo)){
+                            updateOldUserData();
+                        }
+                        else{
+                            storeNewUser();
+                        }
+
+                    } else {
+                        if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                            Toast.makeText(VerifyOTP.this, "Verification Not Completed! Try again.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void updateOldUserData() {
+        String _mobile = getIntent().getStringExtra("mob");
+        String _user = getIntent().getStringExtra("user");
+
+        Intent intent = new Intent(getApplicationContext(),SetPassword.class);
+        intent.putExtra("mob",_mobile);
+        intent.putExtra("user",_user);
+        startActivity(intent);
+        finish();
+    }
+
+    private void storeNewUser() {
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("users");
+        String _user = getIntent().getStringExtra("user");
+        String _mail = getIntent().getStringExtra("mail");
+        String _mobile = getIntent().getStringExtra("mob");
+        String _pass = getIntent().getStringExtra("pass");
+        String _cpass = getIntent().getStringExtra("cpass");
+        UserHeplerClass heplerClass = new UserHeplerClass(_user,_mail,_mobile,_pass,_cpass);
+        reference.child(_user).setValue(heplerClass);
+        Intent intent = new Intent(VerifyOTP.this,MainScreen.class);
+
+        intent.putExtra("user",_user);
+        intent.putExtra("mob",_mobile);
+        intent.putExtra("mail",_mail);
+        intent.putExtra("pass",_pass);
+        intent.putExtra("cpass",_cpass);
+        startActivity(intent);
+
+
+        Toast.makeText(VerifyOTP.this, "Verification Completed!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    public void callnectscreenfromotp(View view){
+//startActivity(new Intent(getApplicationContext(), MainScreen.class));
+//finish();
+        String code = pinview.getText().toString();
+        if(!code.isEmpty()){
+            verifyCode(code);
+        }
+    }
+}
