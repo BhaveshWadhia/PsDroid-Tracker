b25b9b7d5504519026712531d683b505e59332c8
Bhavesh Wadhia <wadhiabhavesh@gmail.com>
2021-04-26 12:40:06 +0530

    Bhavesh Committed Added GoogleMap

--- app/src/main/java/com/example/psdroid/ui/gps/GpsFragment.java
+++ app/src/main/java/com/example/psdroid/ui/gps/GpsFragment.java
@@ -1,6 +1,8 @@
 package com.example.psdroid.ui.gps;
 //Import class
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,39 +12,35 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.example.psdroid.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 //GPS Fragment
 public class
-GpsFragment extends Fragment {
+GpsFragment extends Fragment implements OnMapReadyCallback {
+    GoogleMap map;
+    MapView mapView;
     public GpsFragment() {
         //Constructor
     }
+
     //Inflate view & Enable menus for this fragment
     @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true); // Enable Menu for this fragment
-        View v = inflater.inflate(R.layout.fragment_gps,container,false);
-        assert getSupportMapFragment() != null;
-        SupportMapFragment add_MapSupport = (SupportMapFragment) getSupportMapFragment().findFragmentById(R.id.map);
-        assert add_MapSupport != null;
-        add_MapSupport.getMapAsync(googleMap -> {
-              LatLng xyz = new LatLng(-34,151);  //Place at this location
-              googleMap.addMarker(new MarkerOptions().position(xyz).title("Demo User"));  //Title of the marker
-              googleMap.moveCamera(CameraUpdateFactory.newLatLng(xyz));
-          });
-        return v;
+        return inflater.inflate(R.layout.fragment_gps, container, false);
     }
-    private FragmentManager getSupportMapFragment() {
-        return null;
-    }
-    //Create GPS Tracking Toolbar menu
+
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu_1, @NonNull MenuInflater inflater_1) {
         //Inflate Menu
@@ -50,36 +48,22 @@ GpsFragment extends Fragment {
         inflater_1.inflate(R.menu.gpstracking_menu, menu_1);
         super.onCreateOptionsMenu(menu_1, inflater_1);
     }
-   /*
-    // When view is created
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //Add support for map
-        SupportMapFragment add_MapSupport = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
-        //Async Map
-        add_MapSupport.getMapAsync(googleMap -> {
-            //Create the map
-            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                //On Click add marker
-                @Override
-                public void onMapClick(LatLng latLng) {
-                    MarkerOptions markerOptions = new MarkerOptions();
-                    markerOptions.position(latLng);
-                    markerOptions.title(latLng.latitude+ ":" + latLng.longitude);
-                    googleMap.clear();
-                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
-                    googleMap.addMarker(markerOptions);
-                }
-            });
-        });
-        // makeMap();
+        mapView = view.findViewById(R.id.mapView);
+        if (mapView != null) {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
     }
-    //Create the GPS Tracking Toolbar Menu
-    private void makeMap() {
-        //Initialize Map
-        MapFragment map_view = new MapFragment();
-        //Open & load fragment with map
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        { return; }
+        map.setMyLocationEnabled(true);
     }
-    */
 }
