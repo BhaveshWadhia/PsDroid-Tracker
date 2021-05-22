b2e637d3128167a2f02f926c2b1aae2108cecaf0
karn301 <karnsshinde961@gmail.com>
2021-05-23 00:45:41 +0530

    Karn

--- app/src/main/java/com/example/psdroid/ui/login/SignupTabFragment.java
+++ app/src/main/java/com/example/psdroid/ui/login/SignupTabFragment.java
@@ -8,6 +8,7 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebHistoryItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -83,6 +84,7 @@ public class SignupTabFragment extends Fragment {
             }  else if (txt_mobile.length() < 10){
                 Toast.makeText(getActivity(), "Invalid mobile number!", Toast.LENGTH_SHORT).show();
             }  else if(!user.getText().toString().isEmpty()){
+              //  Toast.makeText(getActivity(), "Entering", Toast.LENGTH_SHORT).show();
 
                 String userEnteredUsername = user.getText().toString().trim();
                // String userEnteredMob = mobile.getText().toString().trim();
@@ -92,36 +94,17 @@ public class SignupTabFragment extends Fragment {
                 checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        //Toast.makeText(getActivity(), "Entering again", Toast.LENGTH_SHORT).show();
                         if (snapshot.exists()) {
-                            Toast.makeText(getActivity(), "Username is already in use!", Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(getActivity(), "Username is already in use!", Toast.LENGTH_SHORT).show();
                             user.requestFocus();
                         }
-                        else {
-                            Query checkmob = reference.child("user").child("mob");
-                            checkmob.addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                    if (snapshot.exists()) {
-                                        Toast.makeText(getActivity(), "Mobile Number is already in use!", Toast.LENGTH_SHORT).show();
-                                        mobile.requestFocus();
-                                    }
-                                    else {
-                                        Intent intent = new Intent(getActivity(), VerifyOTP.class);
+                        else{
+                            //Toast.makeText(getActivity(), "Hii", Toast.LENGTH_SHORT).show();
+                            checkmob(txt_email,txt_mobile,txt_user,txt_pass,txt_conpass);
 
-                                        intent.putExtra("user", txt_user);
-                                        intent.putExtra("mob", txt_mobile);
-                                        intent.putExtra("mail", txt_email);
-                                        intent.putExtra("pass", txt_pass);
-                                        intent.putExtra("cpass", txt_conpass);
-                                        startActivity(intent);
-                                        getActivity().finish();
-                                    }
-                                }
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError error) {
 
-                                }
-                            });/*
+                            /*
                             Intent intent = new Intent(getActivity(), VerifyOTP.class);
 
                             intent.putExtra("user", txt_user);
@@ -138,6 +121,7 @@ public class SignupTabFragment extends Fragment {
 
                     }
                 });
+
 
                       /*  else {
                             Intent intent = new Intent(getActivity(), VerifyOTP.class);
@@ -161,9 +145,48 @@ public class SignupTabFragment extends Fragment {
             /*    UserHeplerClass heplerClass = new UserHeplerClass(txt_email,txt_user,txt_mobile,txt_pass,txt_conpass);
                 reference.child(txt_user).setValue(heplerClass);
                 Toast.makeText(getActivity(), "Registered successfully!", Toast.LENGTH_SHORT).show();*/
-            }
+            }/*
+            else{
+                checkmob(txt_email,txt_mobile,txt_user,txt_pass,txt_conpass);
+                Toast.makeText(getActivity(), "", Toast.LENGTH_SHORT).show();
+            }*/
         });
         return root;
+    }
+
+    private void checkmob(String txt_email, String txt_mobile, String txt_user, String txt_pass, String txt_conpass) {
+        Toast.makeText(getActivity(), "Entering function", Toast.LENGTH_SHORT).show();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users");
+        Query checkmob = ref.orderByChild("mobile");
+
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    String mob = ds.child("mobile").getValue(String.class);
+                    Toast.makeText(getActivity(), ""+mob, Toast.LENGTH_SHORT).show();
+                    if (mob.equals(txt_mobile)) {
+                        Toast.makeText(getActivity(), "Mobile Number is already in use!", Toast.LENGTH_SHORT).show();
+                        mobile.requestFocus();
+                    }
+                    else {
+                        Intent intent = new Intent(getActivity(), VerifyOTP.class);
+
+                        intent.putExtra("user", txt_user);
+                        intent.putExtra("mob", txt_mobile);
+                        intent.putExtra("mail", txt_email);
+                        intent.putExtra("pass", txt_pass);
+                        intent.putExtra("cpass", txt_conpass);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }
+                }}
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        checkmob.addListenerForSingleValueEvent(eventListener);
     }
 
     boolean validateMobile(String input){
