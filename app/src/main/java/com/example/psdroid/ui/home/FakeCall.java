44d016aee10d5d32c981cddc76bce6e208c1ef0a
Bhavesh Wadhia <wadhiabhavesh@gmail.com>
2021-05-29 12:39:54 +0530

    Fake call created

--- app/src/main/java/com/example/psdroid/ui/home/FakeCall.java
+++ app/src/main/java/com/example/psdroid/ui/home/FakeCall.java
@@ -1,13 +1,20 @@
 package com.example.psdroid.ui.home;
 //Import Class
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.psdroid.R;
 //Fake Call
 public class FakeCall extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +23,13 @@ public class FakeCall extends AppCompatActivity {
         Intent intent = getIntent();
         String set_fakename = intent.getStringExtra("fakename_key");
         fakename.setText(set_fakename);
+        Uri ringtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+        Ringtone ringPlayer = RingtoneManager.getRingtone(getApplicationContext(),ringtone);
+        ringPlayer.setLooping(true);
+        ringPlayer.play();
+        new Handler().postDelayed(() -> {
+            ringPlayer.stop();
+            finish();
+        },45000);
     }
 }
