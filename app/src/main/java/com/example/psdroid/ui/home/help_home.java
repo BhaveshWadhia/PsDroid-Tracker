8169dfd36d0eb5b92d1eed80b901c6d24ed7ac80
karn301 <karnsshinde961@gmail.com>
2021-06-04 18:33:29 +0530

    karn

--- app/src/main/java/com/example/psdroid/ui/home/help_home.java
+++ app/src/main/java/com/example/psdroid/ui/home/help_home.java
@@ -26,7 +26,7 @@ public class help_home extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-        
+
         setContentView(R.layout.home_helplayout);
 
         Intent intent = getIntent();
--- app/src/main/java/com/example/psdroid/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/psdroid/ui/notifications/NotificationsFragment.java
@@ -40,6 +40,7 @@ public class NotificationsFragment extends Fragment {
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
